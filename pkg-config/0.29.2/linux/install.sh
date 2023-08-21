bcc9cdb42094c6413c56660e17c11ad3848cdca1
ShreyasVH <shreyas.deve.1@gmail.com>
2023-08-21 20:27:10 +0530

    pkg-config 0.29.2

---
+++ pkg-config/0.29.2/linux/install.sh
@@ -0,0 +1,37 @@
+FOLDER_NAME=pkg-config
+VERSION=0.29.2
+
+if [ ! -d "$HOME/sources" ]; then
+	mkdir "$HOME/sources"
+fi
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
+	mkdir "$HOME/sources/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	wget "https://pkgconfig.freedesktop.org/releases/pkg-config-$VERSION.tar.gz"
+	tar -xvf "pkg-config-$VERSION.tar.gz"
+	mv "pkg-config-$VERSION" $VERSION
+	cd $VERSION
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-internal-glib
+	make
+	sudo make install
+
+	cd $HOME/programs/$FOLDER_NAME/$VERSION
+	sudo chown -R $(whoami) .
+
+	cd $HOME/sources/$FOLDER_NAME
+	rm -rf $VERSION
+	rm "pkg-config-$VERSION.tar.gz"
+fi
+
+cd $HOME/install-files
