5789e059166335811099a969b2245f26af0ad66f
ShreyasVH <shreyas.deve.1@gmail.com>
2023-08-22 11:34:46 +0530

    maven 3.6.3

---
+++ maven/3.6.3/macos/install.sh
@@ -0,0 +1,24 @@
+VERSION=3.6.3
+FOLDER_NAME=maven
+MAJOR_VERSION=3
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	wget "https://archive.apache.org/dist/maven/maven-$MAJOR_VERSION/$VERSION/binaries/apache-maven-$VERSION-bin.tar.gz"
+	tar -xvf "apache-maven-$VERSION-bin.tar.gz"
+	mv "apache-maven-"$VERSION $VERSION
+
+	cd $HOME/programs/$FOLDER_NAME/$VERSION
+	sudo chown -R $(whoami) .
+
+	cd $HOME/sources/$FOLDER_NAME
+	rm -rf $VERSION
+	rm "apache-maven-$VERSION-bin.tar.gz"
+fi
