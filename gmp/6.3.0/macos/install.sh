eb6b3123e49587fea8416ebabcb75254217251de
ShreyasVH <shreyas.deve.1@gmail.com>
2023-09-08 13:46:06 +0530

    gmp 6.3.0 macos

---
+++ gmp/6.3.0/macos/install.sh
@@ -0,0 +1,41 @@
+FOLDER_NAME=gmp
+VERSION=6.3.0
+
+if [ ! -d "$HOME/sources" ]; then
+	mkdir "$HOME/sources"
+fi
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
+	mkdir "$HOME/sources/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME/$VERSION"
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	wget "https://ftp.gnu.org/gnu/gmp/gmp-$VERSION.tar.xz"
+	tar -xvf "gmp-$VERSION.tar.xz"
+	mv "gmp-"$VERSION $VERSION
+	cd $VERSION
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --disable-static
+	make
+	sudo make install
+
+	cd $HOME/programs/$FOLDER_NAME/$VERSION
+	sudo chown -R $(whoami) .
+
+	cd $HOME/sources/$FOLDER_NAME
+	rm -rf $VERSION
+	rm "gmp-$VERSION.tar.xz"
+fi
+
+cd $HOME/install-files
