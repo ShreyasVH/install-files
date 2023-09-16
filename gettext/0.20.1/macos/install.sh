f6b178e8c7ed77212c4c8b3794de027e62bdbd0f
ShreyasVH <shreyas.deve.1@gmail.com>
2023-09-16 11:41:26 +0530

    gettext 0.20.1 macos

---
+++ gettext/0.20.1/macos/install.sh
@@ -0,0 +1,40 @@
+FOLDER_NAME=gettext
+VERSION=0.20.1
+
+if [ ! -d "$HOME/sources" ]; then
+	mkdir "$HOME/sources"
+fi
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
+	mkdir "$HOME/sources/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME/$VERSION"
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	export CC=gcc
+	wget "https://ftp.gnu.org/gnu/gettext/gettext-$VERSION.tar.gz"
+	tar -xvf "gettext-$VERSION.tar.gz"
+	mv "gettext-$VERSION" $VERSION
+	cd $VERSION
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION
+	make
+	sudo make install
+
+	cd $HOME/programs/$FOLDER_NAME/$VERSION
+	sudo chown -R $(whoami) .
+
+	cd $HOME/sources/$FOLDER_NAME
+	rm -rf $VERSION
+	rm "gettext-$VERSION.tar.gz"
+fi
