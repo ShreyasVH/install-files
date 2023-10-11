d66fac95ca7b3764f470c174f3716f98fd25233e
ShreyasVH <shreyas.deve.1@gmail.com>
2023-10-11 17:41:57 +0530

    apache 2.4.56 macos updates

--- apache/2.4.56/macos/install.sh
+++ apache/2.4.56/macos/install.sh
@@ -61,43 +61,43 @@ if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 
 	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
 
-	printf "\t${bold}${blink}${green}Downloading source code${clear}\n"
-	wget -q "https://archive.apache.org/dist/httpd/httpd-$VERSION.tar.gz"
-	printf "\t${bold}${blink}${green}Extracting source code${clear}\n"
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://archive.apache.org/dist/httpd/httpd-$VERSION.tar.gz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
 	tar -xf "httpd-"$VERSION".tar.gz"
 	mv "httpd-"$VERSION $VERSION
 	cd $VERSION
-	printf "\t${bold}${blink}${green}Configuring${clear}\n"
+	printf "\t${bold}${green}Configuring${clear}\n"
 	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
 	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-apr=$HOME/programs/$APR_FOLDER_NAME/$APR_VERSION/bin/apr-1-config --with-apr-util=$HOME/programs/$APR_UTIL_FOLDER_NAME/$APR_UTIL_VERSION/bin/apu-1-config --with-pcre=$HOME/programs/$PCRE_FOLDER_NAME/$PCRE_VERSION/bin/pcre2-config > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
-	printf "\t${bold}${blink}${green}Making${clear}\n"
+	printf "\t${bold}${green}Making${clear}\n"
 	make > $HOME/logs/$FOLDER_NAME/$VERSION/makeOutput.txt 2>&1
-	printf "\t${bold}${blink}${green}Installing${clear}\n"
+	printf "\t${bold}${green}Installing${clear}\n"
 	echo $USER_PASSWORD | sudo -S make install > $HOME/logs/$FOLDER_NAME/$VERSION/installOutput.txt 2>&1
 
-	cd $HOME/programs/$FOLDER_NAME/$VERSION
-	sudo chown -R $(whoami) .
-
-	cp conf/httpd.conf ~
-	rm conf/httpd.conf
-	ln -s ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/macos/httpd.conf conf/httpd.conf
-	cp conf/extra/httpd-vhosts.conf ~
-	rm conf/extra/httpd-vhosts.conf
-	ln -s ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/macos/httpd-vhosts.conf conf/extra/httpd-vhosts.conf
-	echo "<html><body><h1>It works! (version: $VERSION)</h1></body></html>" > htdocs/index.html
-
-	touch .envrc
-	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
-	echo "" >> .envrc
-	direnv allow
-
-	touch start.sh
-	echo "apachectl start" >> start.sh
-
-	touch stop.sh
-	echo 'apachectl stop' >> stop.sh
-
 	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/apachectl" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S chown -R $(whoami) .
+
+		mv conf/httpd.conf ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/macos/httpd.conf.default
+		ln -s ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/macos/httpd.conf conf/httpd.conf
+		mv conf/extra/httpd-vhosts.conf ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/macos/httpd-vhosts.conf.default
+		ln -s ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/macos/httpd-vhosts.conf conf/extra/httpd-vhosts.conf
+		echo "<html><body><h1>It works! (version: $VERSION)</h1></body></html>" > htdocs/index.html
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		touch start.sh
+		echo "apachectl start" >> start.sh
+
+		touch stop.sh
+		echo 'apachectl stop' >> stop.sh
+
+
+		printf "\t${bold}${green}Clearing${clear}\n"
 		cd $HOME/sources/$FOLDER_NAME
 		rm -rf $VERSION
 		rm "httpd-"$VERSION".tar.gz"
