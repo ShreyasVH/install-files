4960987242486339f4aa862833a8dc41086cac1a
ShreyasVH <shreyas.deve.1@gmail.com>
2023-10-14 00:20:30 +0530

    sbt 1.8.1 macos update

--- sbt/1.8.1/macos/install.sh
+++ sbt/1.8.1/macos/install.sh
@@ -7,16 +7,21 @@ fi
 
 if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
 	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
 
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 	cd $HOME/programs/$FOLDER_NAME
 
-	wget "https://github.com/sbt/sbt/releases/download/v$VERSION/sbt-$VERSION.tgz"
-	tar -xvf "sbt-$VERSION.tgz"
-	ls
+	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://github.com/sbt/sbt/releases/download/v$VERSION/sbt-$VERSION.tgz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf "sbt-$VERSION.tgz"
 	mv sbt $VERSION
 
-	cd $HOME/programs/$FOLDER_NAME/$VERSION
-	sudo chown -R $(whoami) .
+	cd $VERSION
+	echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
 
 	cd ..
 	rm "sbt-$VERSION.tgz"
