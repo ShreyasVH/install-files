fe7e00af4f6326614dc06ca2b6816fd31c61b082
ShreyasVH <shreyas.deve.1@gmail.com>
2023-10-20 20:54:35 +0530

    mongosh 2.0.2 macos

---
+++ mongosh/2.0.2/macos/install.sh
@@ -0,0 +1,34 @@
+FOLDER_NAME=mongosh
+VERSION=2.0.2
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	cd $HOME/programs/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://downloads.mongodb.com/compass/mongosh-$VERSION-darwin-arm64.zip"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	unzip "mongosh-$VERSION-darwin-arm64.zip" > /dev/null 2>&1
+	mv "mongosh-$VERSION-darwin-arm64" $VERSION
+	cd $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/mongosh" ]; then
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		printf "\t${bold}${green}Clearing${clear}\n"
+		cd ..
+		rm "mongosh-$VERSION-darwin-arm64.zip"
+	fi
+fi
+
+cd $HOME/install-files
