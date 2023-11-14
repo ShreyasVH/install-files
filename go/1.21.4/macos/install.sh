b4f4cc3fbd24183ee28f7ae9be2bd8a9e62abd53
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-14 21:07:56 +0530

    go 1.21.4 macos

---
+++ go/1.21.4/macos/install.sh
@@ -0,0 +1,35 @@
+VERSION=1.21.4
+FOLDER_NAME=go
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	cd $HOME/programs/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://go.dev/dl/go$VERSION.darwin-arm64.tar.gz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf "go$VERSION.darwin-arm64.tar.gz"
+	mv go $VERSION
+	cd $VERSION
+
+	echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+	touch .envrc
+	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	direnv allow
+
+	printf "\t${bold}${green}Clearing${clear}\n"
+	cd ..
+	rm "go$VERSION.darwin-arm64.tar.gz"
+fi
+
