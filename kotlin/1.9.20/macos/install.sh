c41e753c91468f1da13c3a2b7aabc1c724dfc973
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-14 22:35:35 +0530

    kotlin 1.9.20 macos

---
+++ kotlin/1.9.20/macos/install.sh
@@ -0,0 +1,44 @@
+VERSION=1.9.20
+FOLDER_NAME=kotlin
+
+JAVA_FOLDER_NAME=java
+JAVA_VERSION=17.0.7
+
+INSTALL_FILES_DIR=$HOME/install-files
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	bash $INSTALL_FILES_DIR/$JAVA_FOLDER_NAME/$JAVA_VERSION/macos/install.sh
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://github.com/JetBrains/kotlin/releases/download/v$VERSION/kotlin-compiler-$VERSION.zip"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	unzip "kotlin-compiler-$VERSION.zip" > /dev/null 2>&1
+	mv kotlinc $VERSION
+	cd $VERSION
+
+	echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+	touch .envrc
+	echo 'export PATH=$HOME/programs/'"$JAVA_FOLDER_NAME/$JAVA_VERSION/Contents/Home/bin:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	direnv allow
+
+	printf "\t${bold}${green}Clearing${clear}\n"
+	cd ..
+	rm "kotlin-compiler-$VERSION.zip"
+fi
+
