44c7cf9d1ac6361ed08ee78856966a635666dd1f
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-14 22:30:25 +0530

    scala 2.13.11 macos

---
+++ scala/2.13.11/macos/install.sh
@@ -0,0 +1,44 @@
+VERSION=2.13.11
+FOLDER_NAME=scala
+
+JAVA_FOLDER_NAME=java
+JAVA_VERSION=17.0.7
+
+INSTALL_FILES_DIR=$HOME/install-files
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	bash $INSTALL_FILES_DIR/$JAVA_FOLDER_NAME/$JAVA_VERSION/macos/install.sh
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://downloads.lightbend.com/scala/$VERSION/scala-$VERSION.tgz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf "scala-$VERSION.tgz"
+	mv "scala-$VERSION" $VERSION
+	cd $VERSION
+
+	echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+	touch .envrc
+	echo 'export JAVA_HOME=$HOME/programs/'"$JAVA_FOLDER_NAME/$JAVA_VERSION"'/Contents/Home' >> .envrc
+	echo "" >> .envrc
+	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	direnv allow
+
+	printf "\t${bold}${green}Clearing${clear}\n"
+	cd ..
+	rm "scala-$VERSION.tgz"
+fi
+
