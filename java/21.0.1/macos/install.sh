2253ade99c4e21ff1a085bff39894c93458caa35
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-17 23:43:42 +0530

    java 21.0.1 macos

---
+++ java/21.0.1/macos/install.sh
@@ -0,0 +1,36 @@
+FOLDER_NAME=java
+VERSION=21.0.1
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	cd $HOME/programs/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://github.com/bell-sw/Liberica/releases/download/$VERSION+12/bellsoft-jdk$VERSION+12-macos-aarch64.tar.gz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf "bellsoft-jdk$VERSION+12-macos-aarch64.tar.gz"
+	mv "jdk-$VERSION.jdk" $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/java" ]; then
+		cd $VERSION
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		printf "\t${bold}${green}Clearing${clear}\n"
+		cd ..
+		rm "bellsoft-jdk$VERSION+12-macos-aarch64.tar.gz"
+	fi
+fi
