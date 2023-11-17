a5596a7a8a2133c3f4806872e8580511434a93ba
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-17 22:30:11 +0530

    apr 1.7.4 macos

--- apr/1.7.4/macos/install.sh
+++ apr/1.7.4/macos/install.sh
@@ -34,7 +34,7 @@ if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 
 	cd $HOME/sources/$FOLDER_NAME
 
-	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
 
 	printf "\t${bold}${green}Downloading source code${clear}\n"
 	wget -q --show-progress "https://archive.apache.org/dist/apr/apr-"$VERSION".tar.gz"
