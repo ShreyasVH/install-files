f5ea3ab933dd28f7fdd3e0a0a742f01fb603eeb2
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-21 22:42:18 +0530

    java 20.0.2 macos update

--- java/20.0.2/macos/install.sh
+++ java/20.0.2/macos/install.sh
@@ -16,22 +16,22 @@ if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
 
 	printf "\t${bold}${green}Downloading source code${clear}\n"
-	wget -q --show-progress "https://download.oracle.com/java/$MAJOR_VERSION/archive/jdk-"$VERSION"_macos-aarch64_bin.tar.gz"
+	wget -q --show-progress "https://github.com/bell-sw/Liberica/releases/download/$VERSION+10/bellsoft-jdk$VERSION+10-macos-aarch64.tar.gz"
 	printf "\t${bold}${green}Extracting source code${clear}\n"
-	tar -xf "jdk-"$VERSION"_macos-aarch64_bin.tar.gz"
+	tar -xf "bellsoft-jdk$VERSION+10-macos-aarch64.tar.gz"
 	mv "jdk-$VERSION.jdk" $VERSION
 
-	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/Contents/Home/bin/java" ]; then
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/java" ]; then
 		cd $VERSION
 		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
 
 		touch .envrc
-		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/Contents/Home/bin:"'$PATH' >> .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
 		echo "" >> .envrc
 		direnv allow
 
 		printf "\t${bold}${green}Clearing${clear}\n"
 		cd ..
-		rm "jdk-"$VERSION"_macos-aarch64_bin.tar.gz"
+		rm "bellsoft-jdk$VERSION+10-macos-aarch64.tar.gz"
 	fi
 fi
