c6ddac234c2a22018165ab96a0601f8a423471bd
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-21 21:02:58 +0530

    scala 3.2.2 macos

---
+++ scala/3.2.2/macos/install.sh
@@ -0,0 +1,35 @@
+VERSION=3.2.2
+FOLDER_NAME=scala
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	cd $HOME/programs/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://github.com/lampepfl/dotty/releases/download/$VERSION/scala3-$VERSION.tar.gz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf "scala3-$VERSION.tar.gz"
+	mv "scala3-$VERSION" $VERSION
+	cd $VERSION
+
+	echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+	touch .envrc
+	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	direnv allow
+
+	printf "\t${bold}${green}Clearing${clear}\n"
+	cd ..
+	rm "scala3-$VERSION.tar.gz"
+fi
+
