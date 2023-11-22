cda54388ccf6e0cd5479b2290dae4d13811ee3f0
ShreyasVH <shreyas.deve.1@gmail.com>
2023-11-22 20:53:13 +0530

    elixir 1.15.7 macos

---
+++ elixir/1.15.7/macos/install.sh
@@ -0,0 +1,74 @@
+FOLDER_NAME=elixir
+VERSION=1.15.7
+
+FOLDER_NAME_ERLANG=erlang
+ERLANG_VERSION=26.1.2
+
+INSTALL_FILES_DIR=$HOME/install-files
+
+if [ ! -d "$HOME/sources" ]; then
+	mkdir "$HOME/sources"
+fi
+
+if [ ! -d "$HOME/programs" ]; then
+	mkdir "$HOME/programs"
+fi
+
+if [ ! -d "$HOME/logs" ]; then
+	mkdir "$HOME/logs"
+fi
+
+if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
+	mkdir "$HOME/sources/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/logs/$FOLDER_NAME" ]; then
+	mkdir "$HOME/logs/$FOLDER_NAME"
+fi
+
+if [ ! -d "$HOME/logs/$FOLDER_NAME/$VERSION" ]; then
+	mkdir "$HOME/logs/$FOLDER_NAME/$VERSION"
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	mkdir "$HOME/programs/$FOLDER_NAME/$VERSION"
+
+	bash $INSTALL_FILES_DIR/$FOLDER_NAME_ERLANG/$ERLANG_VERSION/macos/install.sh
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	export PATH=$HOME/programs/$FOLDER_NAME_ERLANG/$ERLANG_VERSION/bin:$PATH
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	git clone -q https://github.com/elixir-lang/elixir.git
+	cd elixir
+	git checkout -q "v"$VERSION
+	printf "\t${bold}${green}Making${clear}\n"
+	make > $HOME/logs/$FOLDER_NAME/$VERSION/makeOutput.txt 2>&1
+	printf "\t${bold}${green}Installing${clear}\n"
+	echo $USER_PASSWORD | sudo -S -p "" make install PREFIX=$HOME/programs/$FOLDER_NAME/$VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/installOutput.txt 2>&1
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/elixir" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME_ERLANG/$ERLANG_VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		printf "\t${bold}${green}Clearing${clear}\n"
+		cd $HOME/sources/$FOLDER_NAME
+		rm -rf elixir
+	fi
+fi
+
+cd $HOME/install-files
