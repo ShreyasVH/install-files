7e368075c1aeb92724e93c44fa17a636f4670667
ShreyasVH <shreyas.deve.1@gmail.com>
2023-12-25 00:44:05 +0530

    python 3.11.6 macos

---
+++ gettext/0.22.2/macos/install.sh
@@ -0,0 +1,35 @@
+FOLDER_NAME=gettext
+VERSION=0.22.2
+
+cd $INSTALL_FILES_DIR
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/gettext" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	export CC=gcc
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	ARCHIVE_FILE="gettext-$VERSION.tar.gz"
+	wget -q --show-progress "https://ftp.gnu.org/gnu/gettext/$ARCHIVE_FILE"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf $ARCHIVE_FILE
+	mv "gettext-$VERSION" $VERSION
+	cd $VERSION
+	printf "\t${bold}${green}Configuring${clear}\n"
+	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --disable-silent-rules --with-included-glib --with-included-libcroco --with-included-libunistring --with-included-libxml --with-emacs --with-lispdir=#{elisp} --disable-java --disable-csharp --without-git --without-cvs --without-xz --with-included-gettext > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/gettext" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p '' chown -R $(whoami) .
+
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+	fi
+fi
+
+cd $HOME/install-files
