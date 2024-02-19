7e74815ee8e6e5ce3049078097b5f7f92c9eb710
ShreyasVH <shreyas.deve.1@gmail.com>
2024-02-19 14:41:19 +0000

    redis 7.0.12 wsl updates

--- redis/7.0.12/wsl/install.sh
+++ redis/7.0.12/wsl/install.sh
@@ -1,29 +1,23 @@
 FOLDER_NAME=redis
 VERSION=7.0.12
 
+cd $INSTALL_FILES_DIR
+
 PKG_CONFIG_FOLDER_NAME=pkg-config
-PKG_CONFIG_VERSION=0.29.2
+PKG_CONFIG_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$PKG_CONFIG_FOLDER_NAME" '.[$folder][$version][$name]')
 
-INSTALL_FILES_DIR=$HOME/install-files
-
-if [ ! -d "$HOME/sources" ]; then
-	mkdir "$HOME/sources"
+if [ ! -e $HOME/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/wsl/redis.conf ]; then
+	printf "redis.conf not found\n"
+	exit
 fi
 
-if [ ! -d "$HOME/programs" ]; then
-	mkdir "$HOME/programs"
+if [ ! -e $HOME/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/wsl/sentinel.conf ]; then
+	printf "sentinel.conf not found\n"
+	exit
 fi
 
-if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
-	mkdir "$HOME/sources/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME/$VERSION"
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/redis-server" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
 
 	bash $INSTALL_FILES_DIR/$PKG_CONFIG_FOLDER_NAME/$PKG_CONFIG_VERSION/wsl/install.sh
 
@@ -31,31 +25,47 @@ if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 
 	export PATH=$HOME/programs/$PKG_CONFIG_FOLDER_NAME/$PKG_CONFIG_VERSION/bin:$PATH
 
-	wget -q "https://github.com/redis/redis/archive/$VERSION.tar.gz"
-	tar -xvf "$VERSION.tar.gz"
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	ARCHIVE_FILE="$VERSION.tar.gz"
+	wget -q --show-progress "https://github.com/redis/redis/archive/$ARCHIVE_FILE"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf $ARCHIVE_FILE
 	mv "redis-$VERSION" $VERSION
 	cd $VERSION
-	make
-	sudo PREFIX=$HOME/programs/$FOLDER_NAME/$VERSION make install
+	
+	bash $INSTALL_FILES_DIR/make.sh $FOLDER_NAME $VERSION
 
-	cd $HOME/programs/$FOLDER_NAME/$VERSION
-	sudo chown -R $(whoami) .
+	printf "\t${bold}${green}Installing${clear}\n"
+	echo $USER_PASSWORD | sudo -S -p '' PREFIX=$HOME/programs/$FOLDER_NAME/$VERSION make install > $HOME/logs/$FOLDER_NAME/$VERSION/installOutput.txt 2>&1
 
-	touch .envrc
-	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
-	echo "" >> .envrc
-	direnv allow
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/redis-server" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p '' chown -R $(whoami) .
 
-	touch start.sh
-	echo "redis-server ~/workspace/myProjects/config-samples/redis/$VERSION/redis.conf" >> start.sh
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
 
-	touch stop.sh
-	echo 'PORT=$(grep '\''^port '\'' ~/workspace/myProjects/config-samples/'"$FOLDER_NAME/$VERSION"'/redis.conf | awk '\''{print $2}'\'')' >> stop.sh
-	echo 'redis-cli -p $PORT shutdown' >> stop.sh
+		touch start.sh
+		mv $HOME/sources/$FOLDER_NAME/$VERSION/redis.conf ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/wsl/redis.conf.default
+		mv $HOME/sources/$FOLDER_NAME/$VERSION/sentinel.conf ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/wsl/sentinel.conf.default
+		echo "redis-server ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/wsl/redis.conf" >> start.sh
+		echo "" >> start.sh
+		echo "redis-sentinel ~/workspace/myProjects/config-samples/$FOLDER_NAME/$VERSION/wsl/sentinel.conf" >> start.sh
+		echo "" >> start.sh
 
-	cd $HOME/sources/$FOLDER_NAME
-	rm -rf $VERSION
-	rm "$VERSION.tar.gz"
+		touch stop.sh
+		echo 'PORT=$(grep '\''^port '\'' ~/workspace/myProjects/config-samples/'"$FOLDER_NAME/$VERSION"'/wsl/redis.conf | awk '\''{print $2}'\'')' >> stop.sh
+		echo 'SENTINEL_PORT=$(grep '\''^port '\'' ~/workspace/myProjects/config-samples/'"$FOLDER_NAME/$VERSION"'/wsl/sentinel.conf | awk '\''{print $2}'\'')' >> stop.sh
+		echo 'kill -9 $(lsof -i:$SENTINEL_PORT -t)' >> stop.sh
+		echo 'redis-cli -p $PORT shutdown' >> stop.sh
+		echo '' >> stop.sh
+
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+	fi
 fi
 
 cd $HOME/install-files
