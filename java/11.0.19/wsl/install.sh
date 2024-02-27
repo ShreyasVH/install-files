11473493ce01aa1ae5c45e11f8673829e4421c0e
ShreyasVH <shreyas.deve.1@gmail.com>
2024-02-27 17:03:23 +0000

    java 11.0.19 wsl updates

--- java/11.0.19/wsl/install.sh
+++ java/11.0.19/wsl/install.sh
@@ -1,25 +1,31 @@
 FOLDER_NAME=java
 VERSION=11.0.19
 
-if [ ! -d "$HOME/programs" ]; then
-	mkdir "$HOME/programs"
-fi
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/java" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 0
 
-if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 	cd $HOME/programs/$FOLDER_NAME
 
-	wget -q "https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/$VERSION+7/openlogic-openjdk-jre-$VERSION+7-linux-x64.tar.gz"
-	tar -xvf "openlogic-openjdk-jre-$VERSION+7-linux-x64.tar.gz"
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	wget -q --show-progress "https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/$VERSION+7/openlogic-openjdk-jre-$VERSION+7-linux-x64.tar.gz"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf "openlogic-openjdk-jre-$VERSION+7-linux-x64.tar.gz"
 	mv "openlogic-openjdk-jre-$VERSION+7-linux-x64" $VERSION
 
-	cd $HOME/programs/$FOLDER_NAME/$VERSION
-	sudo chown -R $(whoami) .
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/java" ]; then
+		cd $VERSION
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
 
-	cd ..
-	rm "openlogic-openjdk-jre-$VERSION+7-linux-x64.tar.gz"
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		printf "\t${bold}${green}Clearing${clear}\n"
+		cd ..
+		rm "openlogic-openjdk-jre-$VERSION+7-linux-x64.tar.gz"
+	fi
 fi
 
