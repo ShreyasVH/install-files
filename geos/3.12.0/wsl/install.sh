08e8b6f57353ea5d43fe64682c82bdb1f8df6183
ShreyasVH <shreyas.deve.1@gmail.com>
2024-03-07 10:00:05 +0530

    geos 3.12.0 wsl updates

--- geos/3.12.0/wsl/install.sh
+++ geos/3.12.0/wsl/install.sh
@@ -1,29 +1,13 @@
 FOLDER_NAME=geos
 VERSION=3.12.0
 
+cd $INSTALL_FILES_DIR
+
 CMAKE_FOLDER_NAME=cmake
-CMAKE_VERSION=3.26.4
+CMAKE_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$CMAKE_FOLDER_NAME" '.[$folder][$version][$name]')
 
-INSTALL_FILES_DIR=$HOME/install-files
-
-if [ ! -d "$HOME/sources" ]; then
-	mkdir "$HOME/sources"
-fi
-
-if [ ! -d "$HOME/programs" ]; then
-	mkdir "$HOME/programs"
-fi
-
-if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
-	mkdir "$HOME/sources/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME/$VERSION"
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/lib/libgeos.so" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
 
 	bash $INSTALL_FILES_DIR/$CMAKE_FOLDER_NAME/$CMAKE_VERSION/wsl/install.sh
 
@@ -31,20 +15,26 @@ if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 
 	cd $HOME/sources/$FOLDER_NAME
 
-	wget "https://download.osgeo.org/geos/geos-$VERSION.tar.bz2"
-	tar -xvf "geos-$VERSION.tar.bz2"
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	ARCHIVE_FILE="geos-$VERSION.tar.bz2"
+	wget -q --show-progress "https://download.osgeo.org/geos/$ARCHIVE_FILE"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf $ARCHIVE_FILE
 	mv "geos-$VERSION" $VERSION
 	cd $VERSION
 	mkdir bld
 	cd bld
-	cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/programs/$FOLDER_NAME/$VERSION
-	make
-	sudo make install
+	printf "\t${bold}${green}Running cmake${clear}\n"
+	cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/programs/$FOLDER_NAME/$VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/cmakeOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
 
-	cd $HOME/programs/$FOLDER_NAME/$VERSION
-	sudo chown -R $(whoami) .
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/lib/libgeos.so" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p '' chown -R $(whoami) .
 
-	cd $HOME/sources/$FOLDER_NAME
-	rm -rf $VERSION
-	rm "geos-$VERSION.tar.bz2"
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+	fi
 fi
