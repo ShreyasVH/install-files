4dfd9718baef7cb28743ad5cc4d7f03d94aaacf8
ShreyasVH <shreyas.deve.1@gmail.com>
2024-03-07 10:10:53 +0530

    sqlite 3.42.0 wsl updates

--- sqlite3/3.42.0/wsl/install.sh
+++ sqlite3/3.42.0/wsl/install.sh
@@ -3,40 +3,32 @@ FOLDER_NAME=sqlite3
 VERSION_FULLFORM=3420000
 VERSION_YEAR=2023
 
-if [ ! -d "$HOME/sources" ]; then
-	mkdir "$HOME/sources"
-fi
+cd $INSTALL_FILES_DIR
 
-if [ ! -d "$HOME/programs" ]; then
-	mkdir "$HOME/programs"
-fi
-
-if [ ! -d "$HOME/sources/$FOLDER_NAME" ]; then
-	mkdir "$HOME/sources/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME"
-fi
-
-if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
-	mkdir "$HOME/programs/$FOLDER_NAME/$VERSION"
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/sqlite3" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
 
 	cd $HOME/sources/$FOLDER_NAME
 
-	wget -q "https://www.sqlite.org/$VERSION_YEAR/sqlite-autoconf-$VERSION_FULLFORM.tar.gz"
-	tar -xvf "sqlite-autoconf-$VERSION_FULLFORM.tar.gz"
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	ARCHIVE_FILE="sqlite-autoconf-$VERSION_FULLFORM.tar.gz"
+	wget -q --show-progress "https://www.sqlite.org/$VERSION_YEAR/$ARCHIVE_FILE"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf $ARCHIVE_FILE
 	mv "sqlite-autoconf-"$VERSION_FULLFORM $VERSION
 	cd $VERSION
-	ls
-	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --disable-static
-	make
-	sudo make install
+	printf "\t${bold}${green}Configuring${clear}\n"
+	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --disable-static > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
 
-	cd $HOME/programs/$FOLDER_NAME/$VERSION
-	sudo chown -R $(whoami) .
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/sqlite3" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p '' chown -R $(whoami) .
 
-	cd $HOME/sources/$FOLDER_NAME
-	rm -rf $VERSION
-	rm "sqlite-autoconf-$VERSION_FULLFORM.tar.gz"
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+	fi
 fi
