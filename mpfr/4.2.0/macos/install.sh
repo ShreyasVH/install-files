fa3d58b4cc3eead4fa57b2adb1dbe74128bc76f6
ShreyasVH <shreyas.deve.1@gmail.com>
2024-06-13 13:32:12 +0530

    mpfr 4.2.0 macos

---
+++ mpfr/4.2.0/macos/install.sh
@@ -0,0 +1,40 @@
+FOLDER_NAME=mpfr
+VERSION=4.2.0
+
+cd $INSTALL_FILES_DIR
+
+GMP_FOLDER_NAME=gmp
+GMP_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$GMP_FOLDER_NAME" '.[$folder][$version][$name]')
+printf $GMP_VERSION
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/lib/libmpfr.dylib" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
+
+	bash $INSTALL_FILES_DIR/$GMP_FOLDER_NAME/$GMP_VERSION/macos/install.sh
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	ARCHIVE_FILE="mpfr-$VERSION.tar.xz"
+	wget -q --show-progress "https://ftp.gnu.org/gnu/mpfr/$ARCHIVE_FILE"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf $ARCHIVE_FILE
+	mv "mpfr-$VERSION" $VERSION
+	cd $VERSION
+	printf "\t${bold}${green}Configuring${clear}\n"
+	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-gmp=$HOME/programs/$GMP_FOLDER_NAME/$GMP_VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/lib/libmpfr.dylib" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+	fi
+fi
+
+cd $HOME/install-files
