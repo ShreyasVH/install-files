2e8be0c20dddceb79cf5ba84c23fd3edf6c9d973
ShreyasVH <shreyas.deve.1@gmail.com>
2024-08-22 10:19:09 +0530

    erlang 27.0.1 macos

---
+++ erlang/27.0.1/macos/install.sh
@@ -0,0 +1,50 @@
+FOLDER_NAME=erlang
+VERSION=27.0.1
+
+cd $INSTALL_FILES_DIR
+
+PERL_FOLDER_NAME=perl
+PERL_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$PERL_FOLDER_NAME" '.[$folder][$version][$name]')
+
+OPENSSL_FOLDER_NAME=openssl
+OPENSSL_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$OPENSSL_FOLDER_NAME" '.[$folder][$version][$name]')
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/erl" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
+
+	bash $INSTALL_FILES_DIR/$PERL_FOLDER_NAME/$PERL_VERSION/macos/install.sh
+	bash $INSTALL_FILES_DIR/$OPENSSL_FOLDER_NAME/$OPENSSL_VERSION/macos/install.sh
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	export PATH=$HOME/programs/$PERL_FOLDER_NAME/$PERL_VERSION/bin:$PATH
+
+	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+
+	printf "\t${bold}${green}Downloading source code${clear}\n"
+	ARCHIVE_FILE="otp_src_$VERSION.tar.gz"
+	wget -q --show-progress "https://github.com/erlang/otp/releases/download/OTP-$VERSION/$ARCHIVE_FILE"
+	printf "\t${bold}${green}Extracting source code${clear}\n"
+	tar -xf $ARCHIVE_FILE
+	mv "otp_src_$VERSION" $VERSION
+	cd $VERSION
+	printf "\t${bold}${green}Configuring${clear}\n"
+	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-ssl=$HOME/programs/$OPENSSL_FOLDER_NAME/$OPENSSL_VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/erl" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+	fi
+fi
+
+cd $HOME/install-files
