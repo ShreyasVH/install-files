2c39f0b8a84958401d5a35f52aab6122cade2007
ShreyasVH <shreyas.deve.1@gmail.com>
2025-01-02 11:51:13 +0000

    haproxy 2.8.2 wsl_24_04

--- getMissingInstallFiles.js
+++ getMissingInstallFiles.js
@@ -14,7 +14,7 @@ const getMissingInslallFiles = (programName, version) => {
 
 	for (const [program, versions] of Object.entries(allRequiredDependencies)) {
 		for (const requiredVersion of versions) {
-			const installFilePath = process.env.HOME + '/install-files/' + program + '/' + requiredVersion + '/macos/install.sh';
+			const installFilePath = process.env.HOME + '/install-files/' + program + '/' + requiredVersion + '/' + process.env.OS_FOLDER + '/install.sh';
 			if (!fs.existsSync(installFilePath)) {
 				if (!missingInstallFiles.hasOwnProperty(program)) {
 					missingInstallFiles[program] = [];
