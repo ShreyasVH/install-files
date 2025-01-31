ce25a04ce7c4aaafb50f2621068a932b0b8f369b
ShreyasVH <shreyas.deve.1@gmail.com>
2025-01-31 16:52:59 +0530

    maven 3.6.3 wsl 18.04 migration

--- wsl_18_04/maven/3.6.3/install.sh
+++ wsl_18_04/maven/3.6.3/install.sh
@@ -5,7 +5,6 @@ VERSION=$(basename $version_dir)
 program_dir=$(dirname "$version_dir")
 FOLDER_NAME=$(basename $program_dir)
 
-
 os_dir=$(dirname $program_dir)
 OS=$(basename $os_dir)
 
@@ -14,39 +13,28 @@ if [ $# -ge 1 ]; then
     DEPTH=$1
 fi
 
-JAVA_FOLDER_NAME=java
-JAVA_VERSION=$(cat "$STATIC_VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$JAVA_FOLDER_NAME" '.[$folder][$version][$name]')
+MAJOR_VERSION=$(echo $VERSION | cut -d '.' -f 1)
 
 source $INSTALL_FILES_DIR/utils.sh
 
 cd $INSTALL_FILES_DIR
 
-
-if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/sbt" ]; then
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
 	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 0
+	
+	cd $HOME/programs/$FOLDER_NAME
 
 	print_message "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}" $((DEPTH))
 
-	bash $INSTALL_FILES_DIR/$OS/$JAVA_FOLDER_NAME/$JAVA_VERSION/install.sh $((DEPTH+1))
-
-	cd $HOME/programs/$FOLDER_NAME
-
 	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
-	ARCHIVE_FILE=sbt-$VERSION.tgz
-	wget -q "https://github.com/sbt/sbt/releases/download/v$VERSION/$ARCHIVE_FILE"
+	ARCHIVE_FILE=apache-maven-$VERSION-bin.tar.gz
+	wget -q "https://archive.apache.org/dist/maven/maven-$MAJOR_VERSION/$VERSION/binaries/$ARCHIVE_FILE"
 	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
 	tar -xf $ARCHIVE_FILE
-	mv sbt $VERSION
+	mv "apache-maven-"$VERSION $VERSION
 
 	cd $VERSION
 	echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
-	
-	touch .envrc
-	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
-	echo "" >> .envrc
-	echo 'export PATH=$HOME/programs/'"$JAVA_FOLDER_NAME/$JAVA_VERSION/bin:"'$PATH' >> .envrc
-	echo "" >> .envrc
-	direnv allow
 
 	print_message "${bold}${green}Clearing${clear}" $((DEPTH))
 	cd ..
