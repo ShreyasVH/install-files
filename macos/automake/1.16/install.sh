18c5ff135c256a5f30fbca840a01ec2badac23e6
ShreyasVH <shreyas.deve.1@gmail.com>
2025-03-14 16:49:35 +0530

    autoconf migrated to macos

--- macos/automake/1.16/install.sh
+++ macos/automake/1.16/install.sh
@@ -1,5 +1,19 @@
-FOLDER_NAME=automake
-VERSION=1.16.5
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
 
 cd $INSTALL_FILES_DIR
 
@@ -9,32 +23,32 @@ AUTOCONF_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" -
 if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/automake" ]; then
 	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
 
-	bash $INSTALL_FILES_DIR/$AUTOCONF_FOLDER_NAME/$AUTOCONF_VERSION/macos/install.sh
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	bash $INSTALL_FILES_DIR/$OS/$AUTOCONF_FOLDER_NAME/$AUTOCONF_VERSION/install.sh $((DEPTH+1))
 
 	export PATH=$HOME/programs/$AUTOCONF_FOLDER_NAME/$AUTOCONF_VERSION/bin:$PATH
 
 	cd $HOME/sources/$FOLDER_NAME
 
-	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
-
-	printf "\t${bold}${green}Downloading source code${clear}\n"
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
 	ARCHIVE_FILE="automake-$VERSION.tar.gz"
-	wget -q --show-progress "https://ftp.gnu.org/gnu/automake/$ARCHIVE_FILE"
-	printf "\t${bold}${green}Extracting source code${clear}\n"
+	wget -q "https://ftp.gnu.org/gnu/automake/$ARCHIVE_FILE"
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
 	tar -xf $ARCHIVE_FILE
 	mv "automake-$VERSION" $VERSION
 	cd $VERSION
-	printf "\t${bold}${green}Configuring${clear}\n"
+	print_message "${bold}${green}Configuring${clear}" $((DEPTH))
 	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
 	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
 	
-	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION $((DEPTH))
 
 	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/automake" ]; then
 		cd $HOME/programs/$FOLDER_NAME/$VERSION
 		echo $USER_PASSWORD | sudo -S -p '' chown -R $(whoami) .
 
-		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE $((DEPTH))
 	fi
 fi
 
