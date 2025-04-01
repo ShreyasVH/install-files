dba64e2e25e2cf1962b917af3fe2d09723be9cb6
ShreyasVH <shreyas.deve.1@gmail.com>
2025-04-01 15:17:32 +0530

    node migrated to macos

---
+++ macos/node/22.2.0/install.sh
@@ -0,0 +1,51 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/node" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 0
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	print_message "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}" $((DEPTH))
+
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
+	ARCHIVE_FILE=node-v$VERSION-darwin-arm64.tar.gz
+	wget -q "https://nodejs.org/dist/v$VERSION/$ARCHIVE_FILE"
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
+	tar -xf $ARCHIVE_FILE
+	mv "node-v$VERSION-darwin-arm64" $VERSION
+	cd $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/node" ]; then
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		print_message "${bold}${green}Clearing${clear}" $((DEPTH))
+		cd ..
+		rm $ARCHIVE_FILE
+	fi
+fi
+
+cd $HOME/install-files
+
+
