99a44ed5b8b0383478baafe39ed99f40fb7bb1e0
ShreyasVH <shreyas.deve.1@gmail.com>
2025-04-21 12:27:22 +0530

    elastic stack migrated to macos

---
+++ macos/elasticsearch/8.14.0/install.sh
@@ -0,0 +1,61 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+if [ ! -e $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/elasticsearch.yml ]; then
+	printf "elasticsearch.yml not found\n"
+	exit
+fi
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/elasticsearch" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 0
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
+	ARCHIVE_FILE=elasticsearch-$VERSION-darwin-x86_64.tar.gz
+	wget -q "https://artifacts.elastic.co/downloads/elasticsearch/$ARCHIVE_FILE"
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
+	tar -xf $ARCHIVE_FILE
+	mv "elasticsearch-$VERSION" $VERSION
+	cd $VERSION
+
+	SUDO_ASKPASS=$HOME/askpass.sh sudo -A chown -R $(whoami) .
+
+	touch .envrc
+	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	direnv allow
+
+	touch start.sh
+	echo "elasticsearch -d > elastic.log 2>&1 &" >> start.sh
+
+	touch stop.sh
+	mv config/elasticsearch.yml ~/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/elasticsearch.yml.default
+	ln -s ~/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/elasticsearch.yml config/elasticsearch.yml
+	echo 'PORT=$(grep '\''http.port: '\'' config/elasticsearch.yml | awk '\''{print $2}'\'')' >> stop.sh
+	echo 'kill -9 $(lsof -t -i:$PORT)' >> stop.sh
+
+	print_message "${bold}${green}Clearing${clear}" $((DEPTH))
+	cd ..
+	rm $ARCHIVE_FILE
+fi
+
+cd $HOME/install-files
