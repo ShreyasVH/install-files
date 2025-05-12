92df92b03ff252480c004f002c6c6ab3d81dcf56
ShreyasVH <shreyas.deve.1@gmail.com>
2025-05-12 12:57:24 +0530

    gzip macos

--- macos/tar/1.35/install.sh
+++ macos/tar/1.35/install.sh
@@ -20,12 +20,16 @@ cd $INSTALL_FILES_DIR
 LIBICONV_FOLDER_NAME=libiconv
 LIBICONV_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$LIBICONV_FOLDER_NAME" '.[$folder][$version][$name]')
 
+GZIP_FOLDER_NAME=gzip
+GZIP_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$GZIP_FOLDER_NAME" '.[$folder][$version][$name]')
+
 if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/tar" ]; then
 	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
 
 	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
 
 	bash $INSTALL_FILES_DIR/$OS/$LIBICONV_FOLDER_NAME/$LIBICONV_VERSION/install.sh $((DEPTH+1))
+	bash $INSTALL_FILES_DIR/$OS/$GZIP_FOLDER_NAME/$GZIP_VERSION/install.sh $((DEPTH+1))
 
 	cd $HOME/sources/$FOLDER_NAME
 
@@ -41,7 +45,7 @@ if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/tar" ]; then
 	cd $VERSION
 	print_message "${bold}${green}Configuring${clear}" $((DEPTH))
 	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
-	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-libiconv-prefix=$HOME/programs/$LIBICONV_FOLDER_NAME/$LIBICONV_VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-libiconv-prefix=$HOME/programs/$LIBICONV_FOLDER_NAME/$LIBICONV_VERSION --with-gzip=$HOME/programs/$GZIP_FOLDER_NAME/$GZIP_VERSION/bin/gzip > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
 	
 	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION $((DEPTH))
 
