405c08abde53b2b7fa5ec57ebedfcd1db6ceba0c
ShreyasVH <shreyas.deve.1@gmail.com>
2025-05-12 23:41:44 +0530

    xz macos

--- macos/tar/1.35/install.sh
+++ macos/tar/1.35/install.sh
@@ -23,6 +23,9 @@ LIBICONV_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" -
 GZIP_FOLDER_NAME=gzip
 GZIP_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$GZIP_FOLDER_NAME" '.[$folder][$version][$name]')
 
+XZ_FOLDER_NAME=xz
+XZ_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$XZ_FOLDER_NAME" '.[$folder][$version][$name]')
+
 if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/tar" ]; then
 	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
 
@@ -30,6 +33,7 @@ if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/tar" ]; then
 
 	bash $INSTALL_FILES_DIR/$OS/$LIBICONV_FOLDER_NAME/$LIBICONV_VERSION/install.sh $((DEPTH+1))
 	bash $INSTALL_FILES_DIR/$OS/$GZIP_FOLDER_NAME/$GZIP_VERSION/install.sh $((DEPTH+1))
+	bash $INSTALL_FILES_DIR/$OS/$XZ_FOLDER_NAME/$XZ_VERSION/install.sh $((DEPTH+1))
 
 	cd $HOME/sources/$FOLDER_NAME
 
@@ -45,13 +49,13 @@ if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/tar" ]; then
 	cd $VERSION
 	print_message "${bold}${green}Configuring${clear}" $((DEPTH))
 	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
-	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-libiconv-prefix=$HOME/programs/$LIBICONV_FOLDER_NAME/$LIBICONV_VERSION --with-gzip=$HOME/programs/$GZIP_FOLDER_NAME/$GZIP_VERSION/bin/gzip > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --with-libiconv-prefix=$HOME/programs/$LIBICONV_FOLDER_NAME/$LIBICONV_VERSION --with-gzip=$HOME/programs/$GZIP_FOLDER_NAME/$GZIP_VERSION/bin/gzip --with-xz=$HOME/programs/$XZ_FOLDER_NAME/$XZ_VERSION/bin/xz > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
 	
 	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION $((DEPTH))
 
 	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/tar" ]; then
 		cd $HOME/programs/$FOLDER_NAME/$VERSION
-		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+		SUDO_ASKPASS=$HOME/askpass.sh sudo -A chown -R $(whoami) .
 
 		touch .envrc
 		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
