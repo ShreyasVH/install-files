58de15967a84772b906072377848edb86a34b725
ShreyasVH <shreyas.deve.1@gmail.com>
2025-05-16 15:03:11 +0000

    updates

---
+++ wsl_24_04/geos/3.11.0/install.sh
@@ -0,0 +1,54 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+CMAKE_FOLDER_NAME=cmake
+CMAKE_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$CMAKE_FOLDER_NAME" '.[$folder][$version][$name]')
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/lib/libgeos.so" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
+
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	bash $INSTALL_FILES_DIR/$OS/$CMAKE_FOLDER_NAME/$CMAKE_VERSION/install.sh $((DEPTH+1))
+
+	export PATH=$HOME/programs/$CMAKE_FOLDER_NAME/$CMAKE_VERSION/bin:$PATH
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
+	ARCHIVE_FILE="geos-$VERSION.tar.bz2"
+	wget -q "https://download.osgeo.org/geos/$ARCHIVE_FILE"
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
+	tar -xf $ARCHIVE_FILE
+	mv "geos-$VERSION" $VERSION
+	cd $VERSION
+	mkdir bld
+	cd bld
+	print_message "${bold}${green}Running cmake${clear}" $((DEPTH))
+	cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/programs/$FOLDER_NAME/$VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/cmakeOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION $((DEPTH))
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/lib/libgeos.so" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		SUDO_ASKPASS=$HOME/askpass.sh sudo -A chown -R $(whoami) .
+
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE $((DEPTH))
+	fi
+fi
--- wsl_24_04/sqlite3/3.39.2/install.sh
+++ wsl_24_04/sqlite3/3.39.2/install.sh
@@ -1,6 +1,9 @@
 version_dir=$(dirname "$(realpath "$0")")
 
 VERSION=$(basename $version_dir)
+MAJOR_VERSION=$(echo $VERSION | cut -d '.' -f 1)
+MINOR_VERSION=$(echo $VERSION | cut -d '.' -f 2)
+PATCH_VERSION=$(echo $VERSION | cut -d '.' -f 3)
 
 program_dir=$(dirname "$version_dir")
 FOLDER_NAME=$(basename $program_dir)
@@ -17,42 +20,37 @@ source $INSTALL_FILES_DIR/utils.sh
 
 cd $INSTALL_FILES_DIR
 
-ODBC_FOLDER_NAME=odbc
-ODBC_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$ODBC_FOLDER_NAME" '.[$folder][$version][$name]')
+VERSION_FULLFORM=$MAJOR_VERSION$MINOR_VERSION"0"$PATCH_VERSION"00"
 
-if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/isql" ]; then
-	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+VERSION_YEAR=$(cat "$STATIC_VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "year" '.[$folder][$version][$name]')
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/sqlite3" ]; then
 	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
-
-	bash $INSTALL_FILES_DIR/$OS/$ODBC_FOLDER_NAME/$ODBC_VERSION/install.sh $((DEPTH+1))
 
 	cd $HOME/sources/$FOLDER_NAME
 
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
 	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
-	ARCHIVE_FILE="unixODBC-$VERSION.tar.gz"
-	wget -q "https://www.unixodbc.org/$ARCHIVE_FILE"
+	ARCHIVE_FILE="sqlite-autoconf-$VERSION_FULLFORM.tar.gz"
+	wget -q "https://www.sqlite.org/$VERSION_YEAR/$ARCHIVE_FILE"
 	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
 	tar -xf $ARCHIVE_FILE
-	mv "unixODBC-$VERSION" $VERSION
+	mv "sqlite-autoconf-"$VERSION_FULLFORM $VERSION
 	cd $VERSION
 	print_message "${bold}${green}Configuring${clear}" $((DEPTH))
 	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
-	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	./configure --prefix=$HOME/programs/$FOLDER_NAME/$VERSION --disable-static > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
 	
 	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION $((DEPTH))
 
-	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/isql" ]; then
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/sqlite3" ]; then
 		cd $HOME/programs/$FOLDER_NAME/$VERSION
 		echo $USER_PASSWORD | sudo -S -p '' chown -R $(whoami) .
 
 		touch .envrc
 		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
-		echo "" >> .envrc
 		direnv allow
-
-		export PATH=$HOME/programs/$FOLDER_NAME/$VERSION/bin:$PATH
-
-		odbcinst -i -d -f /etc/odbcinst.ini > /dev/null 2>&1
 
 		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE $((DEPTH))
 	fi
