8199db63ce1f5eda28721ab9ad5ad146fbed682d
ShreyasVH <shreyas.deve.1@gmail.com>
2025-05-21 10:24:04 +0000

    moved pending files for wsl_20_04

--- wsl_20_04/apr/1.7.3/install.sh
+++ wsl_20_04/apr/1.7.3/install.sh
@@ -1,5 +1,19 @@
-FOLDER_NAME=apr
-VERSION=1.7.3
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
 
 cd $INSTALL_FILES_DIR
 
@@ -8,20 +22,20 @@ if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/apr-1-config" ]; then
 
 	cd $HOME/sources/$FOLDER_NAME
 
-	printf "${bold}${yellow}Installing $FOLDER_NAME $VERSION${clear}\n"
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
 
-	printf "\t${bold}${green}Downloading source code${clear}\n"
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
 	ARCHIVE_FILE="apr-"$VERSION".tar.gz"
-	wget -q --show-progress "https://archive.apache.org/dist/apr/$ARCHIVE_FILE"
-	printf "\t${bold}${green}Extracting source code${clear}\n"
+	wget -q "https://archive.apache.org/dist/apr/$ARCHIVE_FILE"
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
 	tar -xf $ARCHIVE_FILE
 	mv "apr-"$VERSION $VERSION
 	cd $VERSION
-	printf "\t${bold}${green}Configuring${clear}\n"
+	print_message "${bold}${green}Configuring${clear}" $((DEPTH))
 	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
 	./configure --prefix=$HOME/programs/apr/$VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
 	
-	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION
+	bash $INSTALL_FILES_DIR/makeAndInstall.sh $FOLDER_NAME $VERSION $((DEPTH))
 
 	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/apr-1-config" ]; then
 		cd $HOME/programs/$FOLDER_NAME/$VERSION
@@ -32,7 +46,7 @@ if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/apr-1-config" ]; then
 		echo "" >> .envrc
 		direnv allow
 
-		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE $((DEPTH))
 	fi
 fi
 
