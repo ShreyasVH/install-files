4abfa683845cdb588c1460ba61e0fea256e553cf
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-05 17:20:25 +0530

    scripts for gnu versions

--- refreshVersions.js
+++ refreshVersions.js
@@ -8,7 +8,9 @@ const fs = require('fs');
 	for (const [programName, details] of Object.entries(programData)) {
 		if (details.tagsUrl && (!details.versionsLastUpdatedOn || (details.versionsLastUpdatedOn < ((new Date).getTime() - (24 * 3600 * 1000))))) {
 			console.log(programName);
-			await getAllVersions(programName, details.tagsUrl);
+			if (details.tagsUrl.includes('github')) {
+				await getAllVersions(programName, details.tagsUrl);
+			}
 
 			programData[programName].versionsLastUpdatedOn = (new Date()).getTime();
 			fs.writeFileSync(filePath, JSON.stringify(programData, null, ' '));
