4c453b63add6bdce755a7676d877eb60137db124
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-18 20:01:50 +0530

    php 7.2.28

---
+++ docker/php/7.2.28/install.sh
@@ -0,0 +1,39 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+if [ ! -e $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/Dockerfile ]; then
+	print_message "${red}${bold}Dockerfile not found${clear}"
+	exit
+fi
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/start.sh" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 0
+	mkdir $HOME/programs/$FOLDER_NAME/$VERSION
+
+	cd $HOME/programs/$FOLDER_NAME/$VERSION
+
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	cp $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/Dockerfile Dockerfile
+
+	print_message "${bold}${green}Building image${clear}"
+	docker build -t php-7.2.28 .
+	
+	
+fi
