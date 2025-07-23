716d7bb5e641f0fb3c8d3e66d23c021988406004
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-23 15:42:12 +0530

    mongo exporter 0.44.0

---
+++ macos/mongo_exporter/0.44.0/install.sh
@@ -0,0 +1,70 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+# if [ ! -e $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/my.cnf ]; then
+# 	printf "my.cnf not found\n"
+# 	exit
+# fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 1
+
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
+	ARCHIVE_FILE="mongodb_exporter-${VERSION}.darwin-arm64.tar.gz"
+	wget --show-progress "https://github.com/percona/mongodb_exporter/releases/download/v${VERSION}/$ARCHIVE_FILE" > $HOME/logs/$FOLDER_NAME/$VERSION/download.txt 2>&1
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
+	tar -xf $ARCHIVE_FILE
+	mv mongodb_exporter-${VERSION}.darwin-arm64 $VERSION
+	cd $VERSION
+
+	SUDO_ASKPASS=$HOME/askpass.sh sudo -A chown -R $(whoami) .
+
+	touch .envrc
+	echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION:"'$PATH' >> .envrc
+	echo "" >> .envrc
+	direnv allow
+
+	# cp $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/my.cnf ./
+
+	touch start.sh
+	PORT=1446
+	echo "PORT=$PORT" >> start.sh
+	echo '' >> start.sh
+	echo 'if ! lsof -i :$PORT > /dev/null; then' >> start.sh
+	echo -e '\techo "Starting"' >> start.sh
+	echo -e "\tmongodb_exporter --mongodb.uri=\"mongodb://localhost:${MONGO_PORT}\" --web.listen-address=\":${PORT}\" --collect-all > exporter.log 2>&1 &" >> start.sh
+	echo 'fi' >> start.sh
+
+	touch stop.sh
+	echo "PORT=$PORT" >> stop.sh
+	echo '' >> stop.sh
+	echo 'if lsof -i :$PORT > /dev/null; then' >> stop.sh
+	echo -e '\techo "Stopping"' >> stop.sh
+	echo -e '\tkill -9 $(lsof -t -i:$PORT)' >> stop.sh
+	echo 'fi' >> stop.sh
+
+	print_message "${bold}${green}Clearing${clear}" $((DEPTH))
+	cd ..
+	rm $ARCHIVE_FILE
+fi
+
