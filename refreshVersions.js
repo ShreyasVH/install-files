d65b0c0d1d9af1ba8c6ddf240fe4a395cecf2079
ShreyasVH <shreyas.deve.1@gmail.com>
2025-09-07 13:10:48 +0530

    intermittant changes

--- refreshVersions.js
+++ refreshVersions.js
@@ -1,4 +1,7 @@
-const getAllVersions = require('./getVersions.js').getAllVersions;
+const getAllVersionsGithub = require('./src/getVersionsScripts/github.js').getAllVersions;
+const getAllVersionsGnu = require('./src/getVersionsScripts/gnu.js').getAllVersions;
+const getAllVersionsPecl = require('./src/getVersionsScripts/pecl.js').getAllVersions;
+const capitalize = require('./src/utils').capitalize;
 const fs = require('fs');
 
 
@@ -8,12 +11,15 @@ const fs = require('fs');
 	for (const [programName, details] of Object.entries(programData)) {
 		if (details.tagsUrl && (!details.versionsLastUpdatedOn || (details.versionsLastUpdatedOn < ((new Date).getTime() - (24 * 3600 * 1000))))) {
 			console.log(programName);
-			if (details.tagsUrl.includes('github')) {
-				await getAllVersions(programName, details.tagsUrl);
+			if (details.scriptName) {
+				const scriptName = `getAllVersions${capitalize(details.scriptName)}`;
+				console.log(scriptName);
+				// await getAllVersions(programName, details.tagsUrl);
 			}
+			
 
-			programData[programName].versionsLastUpdatedOn = (new Date()).getTime();
-			fs.writeFileSync(filePath, JSON.stringify(programData, null, ' '));
+			// programData[programName].versionsLastUpdatedOn = (new Date()).getTime();
+			// fs.writeFileSync(filePath, JSON.stringify(programData, null, ' '));
 		}
 	}
 })();
