bb36037b99826e8dc52cf68c9d255a76d085cfa0
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-04-17 11:47:16 +0530

    support for ubuntu 24.04
    * support for ubuntu 24 04
    
    * removed invalid line
    
    * added support for more programs ubuntu 24 04
    
    * added more programs
    
    * more programs
    
    * apache
    
    * more programs
    
    * making scripts depend on pid

---
+++ ubuntu_24_04/jenkins/2.554/createMultibranchPipeline.groovy
@@ -0,0 +1,54 @@
+#!groovy
+
+import jenkins.model.*
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+import jenkins.branch.*
+import jenkins.scm.api.*
+import org.jenkinsci.plugins.github_branch_source.*
+import com.cloudbees.hudson.plugins.folder.computed.DefaultOrphanedItemStrategy
+import jenkins.branch.buildstrategies.basic.*
+
+def j = Jenkins.instance
+
+def jobName = args[0].toString()
+def repoOwner = args[1].toString()
+def repository = args[2].toString()
+
+
+def mbp = j.getItem(jobName)
+if (mbp == null) {
+  mbp = j.createProject(WorkflowMultiBranchProject, jobName)
+}
+
+// Configure branch source (GitHub)
+def githubSource = new GitHubSCMSource(repoOwner, repository)
+githubSource.setCredentialsId("github-creds")     // Jenkins credential id
+githubSource.setId("github-${repoOwner}-${repository}")
+
+githubSource.traits = [
+  new BranchDiscoveryTrait(1),
+
+  new OriginPullRequestDiscoveryTrait(2),
+
+  new ForkPullRequestDiscoveryTrait(2, new ForkPullRequestDiscoveryTrait.TrustPermission())
+]
+
+def branchSource = new BranchSource(githubSource)
+branchSource.setBuildStrategies([
+  new SkipInitialBuildOnFirstBranchIndexing()
+] as List<BranchBuildStrategy>)
+
+mbp.getSourcesList().clear()
+mbp.getSourcesList().add(branchSource)
+
+mbp.setOrphanedItemStrategy(new DefaultOrphanedItemStrategy(true, "", ""))
+
+// mbp.addTrigger(new com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger("1m"))
+
+mbp.save()
+
+// println "Created/updated multibranch job: ${jobName}"
+
+mbp.scheduleBuild2(0)
+
+// println "Indexing finished"
