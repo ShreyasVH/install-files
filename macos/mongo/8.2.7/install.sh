91c29892d903a6af8d6733c5a688a35c0fe0066c
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-05-20 07:08:30 +0530

    2026 05 09 (#68)
    * version updates
    
    * updates
    
    * version updates
    
    * updates
    
    * corrected binding command
    
    * library upgrades
    
    * node upgrade
    
    * node upgrade
    
    * updates for ubuntu_24_04

---
+++ macos/mongo/8.2.7/install.sh
@@ -0,0 +1,94 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+MONGO_SH_FOLDER_NAME=mongosh
+MONGO_SH_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$MONGO_SH_FOLDER_NAME" '.[$folder][$version][$name]')
+
+if [ ! -e $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/mongod.conf ]; then
+	printf "mongod.conf not found\n"
+	exit
+fi
+
+if [ ! -d "$HOME/programs/$FOLDER_NAME/$VERSION" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 0 0
+
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	cd $HOME/programs/$FOLDER_NAME
+
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
+	ARCHIVE_FILE=mongodb-macos-arm64-$VERSION.tgz
+	wget -q "https://fastdl.mongodb.org/osx/$ARCHIVE_FILE"
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
+	tar -xf $ARCHIVE_FILE
+	mv "mongodb-macos-aarch64--$VERSION" $VERSION
+	cd $VERSION
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/mongod" ]; then
+		echo $USER_PASSWORD | sudo -S -p "" chown -R $(whoami) .
+
+		mkdir data
+		mkdir logs
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		ln -s $HOME/workspace/myProjects/config-samples/$OS/$FOLDER_NAME/$VERSION/mongod.conf mongod.conf
+		touch start.sh
+		echo 'PORT=$(grep '\''port: '\'' mongod.conf | awk '\''{print $2}'\'')' >> start.sh
+		echo '' >> start.sh
+		echo 'if [ ! -e mongod.pid ]; then' >> start.sh
+		echo -e '\techo "Starting"' >> start.sh
+		echo -e "\tmongod -f mongod.conf > mongo.log 2>&1 &" >> start.sh
+		echo 'fi' >> start.sh
+
+		touch stop.sh
+		PORT=$(grep 'port: ' mongod.conf | awk '{print $2}')
+		echo 'PORT=$(grep '\''port: '\'' mongod.conf | awk '\''{print $2}'\'')' >> stop.sh
+		echo '' >> stop.sh
+		echo 'if [ -e mongod.pid ]; then' >> stop.sh
+		echo -e '\techo "Stopping"' >> stop.sh
+		echo -e '\tkill -9 $(cat mongod.pid)' >> stop.sh
+		echo -e '\trm mongod.pid' >> stop.sh
+		echo 'fi' >> stop.sh
+
+		print_message "${bold}${green}Clearing${clear}" $((DEPTH))
+		cd ..
+		rm $ARCHIVE_FILE
+
+		export PATH=$HOME/programs/$FOLDER_NAME/$VERSION/bin:$PATH
+		cd $VERSION
+		bash start.sh > /dev/null 2>&1
+
+		bash $INSTALL_FILES_DIR/$OS/$MONGO_SH_FOLDER_NAME/$MONGO_SH_VERSION/install.sh  $((DEPTH+1))
+
+		print_message "${bold}${green}Waiting for server start${clear}" $((DEPTH))
+		while [[ ! $(lsof -i:$PORT -t | wc -l) -gt 0 ]];
+		do
+			:
+		done
+
+		export PATH=$HOME/programs/$MONGO_SH_FOLDER_NAME/$MONGO_SH_VERSION/bin:$PATH
+		mongosh --eval 'rs.initiate({_id: "myReplicaSet", members: [{ _id: 0, host: "127.0.0.1:'$PORT'" }]})' "mongodb://127.0.0.1:$PORT" > rsInitiateLog.txt
+		bash stop.sh > /dev/null 2>&1
+	fi
+fi
+
---
+++ macos/python/3.14.4/install.sh
@@ -0,0 +1,71 @@
+version_dir=$(dirname "$(realpath "$0")")
+
+VERSION=$(basename $version_dir)
+
+program_dir=$(dirname "$version_dir")
+FOLDER_NAME=$(basename $program_dir)
+
+os_dir=$(dirname $program_dir)
+OS=$(basename $os_dir)
+
+DEPTH=1
+if [ $# -ge 1 ]; then
+    DEPTH=$1
+fi
+
+source $INSTALL_FILES_DIR/utils.sh
+
+cd $INSTALL_FILES_DIR
+
+PKG_CONFIG_FOLDER_NAME=pkg-config
+PKG_CONFIG_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$PKG_CONFIG_FOLDER_NAME" '.[$folder][$version][$name]')
+
+GETTEXT_FOLDER_NAME=gettext
+GETTEXT_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$GETTEXT_FOLDER_NAME" '.[$folder][$version][$name]')
+
+OPENSSL_FOLDER_NAME=openssl
+OPENSSL_VERSION=$(cat "$VERSION_MAP_PATH" | jq -r --arg folder "$FOLDER_NAME" --arg version "$VERSION" --arg name "$OPENSSL_FOLDER_NAME" '.[$folder][$version][$name]')
+
+if [ ! -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/python3" ]; then
+	bash $INSTALL_FILES_DIR/createRequiredFolders.sh $FOLDER_NAME $VERSION 1 1
+
+	print_message "${bold}${yellow}Installing ${FOLDER_NAME} ${VERSION}${clear}" $((DEPTH))
+
+	bash $INSTALL_FILES_DIR/$OS/$PKG_CONFIG_FOLDER_NAME/$PKG_CONFIG_VERSION/install.sh $((DEPTH+1))
+	bash $INSTALL_FILES_DIR/$OS/$GETTEXT_FOLDER_NAME/$GETTEXT_VERSION/install.sh $((DEPTH+1))
+	bash $INSTALL_FILES_DIR/$OS/$OPENSSL_FOLDER_NAME/$OPENSSL_VERSION/install.sh $((DEPTH+1))
+
+	cd $HOME/sources/$FOLDER_NAME
+
+	export PATH="$HOME/programs/$PKG_CONFIG_FOLDER_NAME/$PKG_CONFIG_VERSION/bin:$PATH"
+
+	export LDFLAGS="-L$HOME/programs/$GETTEXT_FOLDER_NAME/$GETTEXT_VERSION/lib"
+	export CPPFLAGS="-I$HOME/programs/$GETTEXT_FOLDER_NAME/$GETTEXT_VERSION/include"
+
+	print_message "${bold}${green}Downloading source code${clear}" $((DEPTH))
+	ARCHIVE_FILE="Python-"$VERSION".tgz"
+	wget --show-progress "https://www.python.org/ftp/python/"$VERSION"/$ARCHIVE_FILE" > $HOME/logs/$FOLDER_NAME/$VERSION/download.txt 2>&1
+	print_message "${bold}${green}Extracting source code${clear}" $((DEPTH))
+	tar -xf $ARCHIVE_FILE
+	mv "Python-"$VERSION $VERSION
+	cd $VERSION
+	print_message "${bold}${green}Configuring${clear}" $((DEPTH))
+	./configure --help > $HOME/logs/$FOLDER_NAME/$VERSION/configureHelp.txt 2>&1
+	./configure --with-pydebug --prefix="$HOME/programs/python/$VERSION" --with-openssl=$HOME/programs/$OPENSSL_FOLDER_NAME/$OPENSSL_VERSION > $HOME/logs/$FOLDER_NAME/$VERSION/configureOutput.txt 2>&1
+	print_message "${bold}${green}Making${clear}" $((DEPTH))
+	make -s -j$PROCESS_COUNT > $HOME/logs/$FOLDER_NAME/$VERSION/makeOutput.txt 2>&1
+	
+	bash $INSTALL_FILES_DIR/install.sh $FOLDER_NAME $VERSION $((DEPTH))
+
+	if [ -e "$HOME/programs/$FOLDER_NAME/$VERSION/bin/python3" ]; then
+		cd $HOME/programs/$FOLDER_NAME/$VERSION
+		SUDO_ASKPASS=$HOME/askpass.sh sudo -A chown -R $(whoami) .
+
+		touch .envrc
+		echo 'export PATH=$HOME/programs/'"$FOLDER_NAME/$VERSION/bin:"'$PATH' >> .envrc
+		echo "" >> .envrc
+		direnv allow
+
+		bash $INSTALL_FILES_DIR/clearSourceFolders.sh $FOLDER_NAME $VERSION $ARCHIVE_FILE $((DEPTH))
+	fi
+fi
