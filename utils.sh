d94553df988990cd2c2f965c41defcc14780648e
ShreyasVH <shreyas.deve.1@gmail.com>
2026-09-20 15:59:22 +0000

    postgres 18.6 archlinux

--- utils.sh
+++ utils.sh
@@ -9,3 +9,24 @@ print_message () {
 	# printf "%s\n" "$MESSAGE"
 	printf "${MESSAGE}\n"
 }
+
+resolve_dependency_version () {
+	local FOLDER_NAME=$1
+	local VERSION=$2
+	local OS=$3
+	local DEPENDENCY=$4
+
+	jq -sr \
+        --arg folder "$FOLDER_NAME" \
+        --arg version "$VERSION" \
+        --arg os "$OS" \
+        --arg dependency "$DEPENDENCY" \
+        '
+        (
+            (.[0][$folder][$version] // {})
+            * (.[1][$folder][$version] // {})
+            * (.[1][$os][$folder][$version] // {})
+        )[$dependency] // empty
+        ' \
+        versionMap.json staticVersionMap.json
+}
